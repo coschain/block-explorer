8866d85bbfe0b84c9c409defb20514ac6149af44
aprocysanae <axdiaoqi220@gmail.com>
2019-03-19 19:06:01 +0800

    prod ip

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    VUE_APP_CHAIN: '"http://34.230.216.41:8080"'
+    // VUE_APP_CHAIN: '"http://34.230.216.41:8080"'
+    VUE_APP_CHAIN: '"http://34.200.75.66:8080"'
     // VUE_APP_CHAIN: '"http://localhost:8080"'
 }
