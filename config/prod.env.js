b9c619cb1ae4caec69193d248e187433391aaa78
aprocysanae <axdiaoqi220@gmail.com>
2019-03-21 20:21:00 +0800

    https

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,7 @@
 module.exports = {
     NODE_ENV: '"production"',
     // VUE_APP_CHAIN: '"http://34.230.216.41:8080"'
-    VUE_APP_CHAIN: '"http://34.200.75.66:8080"'
+    // VUE_APP_CHAIN: '"http://34.200.75.66:8080"'
+    VUE_APP_CHAIN: '"https://testnode.contentos.io"'
     // VUE_APP_CHAIN: '"http://localhost:8080"'
 }
