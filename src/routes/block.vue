ded0768c22ceddc451d3dc683691919197e52dbd
wangchao <wangchao4055@gmail.com>
2019-03-21 12:11:13 +0800

    delete useless log in block vue

--- src/routes/block.vue
+++ src/routes/block.vue
@@ -190,8 +190,6 @@
                 this.blockInfo = blkInfo[0];
                 this.bTime = this.blockInfo.toObject().timestamp.utcSeconds*1000;
                 this.blkHash = this.blockInfo.getBlockId().getHexHash();
-                console.log("block height is:");
-                console.log(this.blockInfo.getBlockHeight());
             }
             this.$root.showModalLoading = false;
           }
