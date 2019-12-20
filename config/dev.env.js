9b0315fd3fcf28f97463fee81f282738365c02e9
aprocysanae <axdiaoqi220@gmail.com>
2019-12-20 19:46:36 +0800

    elk address

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
     VUE_APP_CHAIN: '"http://localhost:8080"',
-    // VUE_ELK_PROXY: '"http://localhost:7475"'
-    VUE_ELK_PROXY: '"http://explorer.contentos.io"'
+    VUE_ELK_PROXY: '"http://localhost:7475"'
 })
