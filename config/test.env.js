3bf3fdaa6d9ada37eaec721d3cffb4d60edee351
Jeff <jeff.lo@contentos.io>
2025-03-11 16:17:52 +0800

    Use https for QA env

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: '"test"',
     VUE_APP_CHAIN: '"https://testnode.contentos.io"',
     VUE_ELK_PROXY: '""',
-    backendOrigin: '"http://qa.cos.tv"',
+    backendOrigin: '"https://qa.cos.tv"',
 }
